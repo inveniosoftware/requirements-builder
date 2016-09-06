a06125a3c0eb3df65a8b9b0a3630ae029fe16813
Yoan Blanc <yoan@dosimple.ch>
2016-09-06 22:31:51 +0200

    global: prefill globals with the '__file__' constant
    * Fix problem when the setup.py command plays with `__file__` to read,
      exec, or whatever.
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- tests/data/setup.py
+++ tests/data/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Requirements-Builder
-# Copyright (C) 2015 CERN.
+# Copyright (C) 2015, 2016 CERN.
 #
 # Requirements-Builder is free software; you can redistribute it and/or
 # modify it under the terms of the Revised BSD License; see LICENSE
@@ -10,6 +10,10 @@
 """Build requirements files from setup.py requirements."""
 
 from setuptools import setup
+
+import os
+
+dirname = os.path.dirname(__file__)
 
 requirements = [
     'click>=5.0.0',
