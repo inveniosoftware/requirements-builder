d104e647f897abb660a4e62fa973e0ce0ffd6193
Antonio Vivace <avivace4@gmail.com>
2020-12-08 15:36:32 +0100

    global: migrate from Travis CI to GitHub Actions

--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,19 @@
-# This file is part of Requirements-Builder
-# Copyright (C) 2015, 2016 CERN.
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 #
-# Requirements-Builder is free software; you can redistribute it and/or
-# modify it under the terms of the Revised BSD License; see LICENSE
-# file for more details.
+# This file is part of Invenio.
+# Copyright (C) 2015-2020 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle requirements_builder && \
-#isort -rc -c -vb -df -sp .isort.cfg **/*.py && \
-check-manifest && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m pytest
+python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
