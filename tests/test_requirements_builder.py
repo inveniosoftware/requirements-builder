cf41f29f3b7353a44b257b4bc268d2f6848290f8
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-07-21 09:40:10 +0200

    Add basic tests for pep621

--- tests/test_requirements_builder.py
+++ tests/test_requirements_builder.py
@@ -9,12 +9,20 @@
 #
 """Tests for `requirements-builder` module."""
 
+import sys
 from os.path import abspath, dirname, join
 
 from requirements_builder import __version__, iter_requirements
 
 REQ = abspath(join(dirname(__file__), "./fixtures/requirements.devel.txt"))
 SETUP = abspath(join(dirname(__file__), "./fixtures/setup.txt"))
+
+
+def _has_toml_lib():
+    if sys.version_info.major == 3 and sys.version_info.minor < 11:
+        return False
+    else:
+        return True
 
 
 def test_version():
@@ -50,17 +58,80 @@ def test_iter_requirements_cfg():
     # Min
     with open(setup) as f:
         with open(setup_cfg) as g:
+            expected = ['click==7.0', 'mock==1.3.0']
+            if not _has_toml_lib():
+                expected.append("tomli==2.0.0")
             assert list(iter_requirements("min", [], '', f, g)) == \
-                ['click==7.0', 'mock==1.3.0']
+                expected
 
     # PyPI
     with open(setup) as f:
         with open(setup_cfg) as g:
+            expected = ['click>=7.0', 'mock<4,>=1.3.0']
+            if not _has_toml_lib():
+                expected.append("tomli>=2.0.0")
             assert list(iter_requirements("pypi", [], '', f, g)) == \
-                ['click>=7.0', 'mock<4,>=1.3.0']
+                expected
 
     # Dev
     with open(setup) as f:
         with open(setup_cfg) as g:
+            expected = ['click>=7.0', 'mock<4,>=1.3.0']
+            if not _has_toml_lib():
+                expected.append("tomli>=2.0.0")
             assert list(iter_requirements("dev", [], req, f, g)) == \
-                ['click>=7.0', 'mock<4,>=1.3.0']
+                expected
+
+
+def test_iter_requirements_toml():
+    """Test requirements-builder for
+    pyproject.toml (pep621)."""
+
+    setup_py = abspath(
+        join(dirname(__file__), "fixtures", "setup_py_placeholder.txt")
+        )
+    pyproject_toml = abspath(
+        join(dirname(__file__), "fixtures", "pyproject.toml")
+        )
+    req = abspath(join(dirname(__file__), "../requirements.devel.txt"))
+
+    # Min
+    with open(setup_py) as setup_py_fp:
+        with open(pyproject_toml, 'rb') as pyproject_toml_fp:
+            expected = ['click==7.0', 'mock==1.3.0']
+            if not _has_toml_lib():
+                expected.append("tomli==2.0.0")
+            assert list(iter_requirements(
+                    "min",
+                    [],
+                    '',
+                    setup_fp=setup_py_fp,
+                    pyproject_toml_fp=pyproject_toml_fp
+                    )) == expected
+    # # PyPI
+    with open(setup_py) as setup_py_fp:
+        with open(pyproject_toml, 'rb') as pyproject_toml_fp:
+            expected = ['click>=7.0', 'mock<4,>=1.3.0']
+            if not _has_toml_lib():
+                expected.append("tomli>=2.0.0")
+            assert list(iter_requirements(
+                    "pypi",
+                    [],
+                    '',
+                    setup_fp=setup_py_fp,
+                    pyproject_toml_fp=pyproject_toml_fp
+                    )) == expected
+
+    # Dev
+    with open(setup_py) as setup_py_fp:
+        with open(pyproject_toml, 'rb') as pyproject_toml_fp:
+            expected = ['click>=7.0', 'mock<4,>=1.3.0']
+            if not _has_toml_lib():
+                expected.append("tomli>=2.0.0")
+            assert list(iter_requirements(
+                    "dev",
+                    [],
+                    req,
+                    setup_fp=setup_py_fp,
+                    pyproject_toml_fp=pyproject_toml_fp
+                    )) == expected
